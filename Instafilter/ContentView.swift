7510b9175449ac0d6b1023f772162f59d3431cb4
Kenneth Rathbun <kenrathbun1@gmail.com>
2024-05-07 01:32:18 -0700

    Built the basic UI

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -5,18 +5,50 @@
 //  Created by Kenneth Oliver Rathbun on 5/3/24.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                if let processedImage {
+                    processedImage
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+                .padding(.vertical)
+                
+                HStack {
+                    Button("Change Filter", action: changeFilter)
+                    
+                    Spacer()
+                    
+                    // share the picture
+                }
+            }
+            .padding([.bottom, .horizontal])
+            .navigationTitle("Instafilter")
         }
-        .padding()
     }
+    
+    func changeFilter () { }
 }
 
 #Preview {
