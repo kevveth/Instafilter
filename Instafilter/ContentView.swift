ed74e947240e37312d403707d5998e15edf797fe
Kenneth Rathbun <kenrathbun1@gmail.com>
2024-05-08 01:59:03 -0700

    Implemented Share link and updated to ask for user review after some amount of uses

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -8,6 +8,7 @@
 import CoreImage
 import CoreImage.CIFilterBuiltins
 import PhotosUI
+import StoreKit
 import SwiftUI
 
 struct ContentView: View {
@@ -22,6 +23,9 @@ struct ContentView: View {
     @State private var showingFilters = false
     
     @State private var beginImage: CIImage?
+    
+    @AppStorage("filterCount") var filterCount = 0
+    @Environment(\.requestReview) var requestReview
     
     var body: some View {
         NavigationStack {
@@ -64,7 +68,9 @@ struct ContentView: View {
                     
                     Spacer()
                     
-                    // share the picture
+                    if let processedImage {
+                        ShareLink(item: processedImage, preview: SharePreview("Instafilter image", image: processedImage))
+                    }
                 }
             }
             .tint(.purple)
@@ -104,9 +110,15 @@ struct ContentView: View {
         }
     }
     
+    @MainActor 
     func setFilter(_ filter: CIFilter) {
         currentFilter = filter
         loadImage()
+        
+        filterCount += 1
+        if filterCount >= 3 {
+            requestReview()
+        }
     }
 }
 
